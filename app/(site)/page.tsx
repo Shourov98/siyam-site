92c0cf46196aea32a81c5483fcb81f139f6b7db2
Shourov98 <mdmahbub0412@gmail.com>
2026-02-20 11:25:42 +0600

    fix(site): layout and route

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,21 +1,17 @@
 import CoreModules from "./components/CoreModules";
-import Footer from "./components/Footer";
 import Features from "./components/Features";
 import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
 import PricingSection from "./components/PricingSection";
 import TestimonialSection from "./components/TestimonialSection";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <Navbar />
       <Hero />
       <Features />
       <CoreModules />
       <TestimonialSection />
       <PricingSection />
-      <Footer />
     </div>
   );
 }
